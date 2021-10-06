1d01ac7a2b659c3607089c3bddd3d9f2a3d5cfed
Carol <caires.c26@gmail.com>
2021-10-06 18:06:21 -0300

    Adicionando exercícios propostos

---
+++ ex5.py
@@ -0,0 +1,3 @@
+num =float(input("Digite a metragem para conversão: "))
+cent=100
+print(num, "metros tem ",num*cent,"centímetros")
---
+++ ex6.py
@@ -0,0 +1,4 @@
+pi = 3.14
+raio = float(input("Digite o valor do raio: "))
+area= float (pi*(raio**2))
+print(area)
